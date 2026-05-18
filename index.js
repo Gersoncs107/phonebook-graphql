e79f000082a44a4704060d96d02d6214d32943a9
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-17 22:20:40 -0300

    phonebook-graphql-update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const connectToDatabase = require('./db')
 const startServer = require('./server')
 
+const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT || 4000
 
 startServer(PORT)
