478741d63aa28da8935a456d16eeb3e1eb379f4d
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-20 22:43:09 -0300

    phonebook-graphql-update

--- resolvers.js
+++ resolvers.js
@@ -87,6 +87,24 @@ const resolvers = {
         })
       })
     },
+    login: async (root, args) => {
+    const user = await User.findOne({ username: args.username })
+
+    if ( !user || args.password !== 'secret' ) {
+      throw new GraphQLError('wrong credentials', {
+        extensions: {
+          code: 'BAD_USER_INPUT'
+        }
+      })        
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    }
+
+    return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    },
   },
 }
 
